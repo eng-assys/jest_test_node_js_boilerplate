8d457e9f75e9ff36df30510556fde6e8189f451f
Lucas Assis <eng_assys@MacBook-Pro-de-Lucas.local>
2020-03-29 12:39:59 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Testes no NodeJS aplicando TDD com Jest
+
+## Aplicação do Curso
+
+* https://www.youtube.com/watch?v=2G_mWfG0DZE
---
+++ src/config/database.js
@@ -0,0 +1,20 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+})
+
+module.exports = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT || "postgres",
+  storage: './__tests__/database.sqlite',
+  operatorsAliases: false,
+  logging: true,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
